7c1c7a9c117fe45814b23d1e5303b9981d2d00ac
Polentzi Saavedra <75914262+polentzisb@users.noreply.github.com>
2023-05-19 22:30:41 -0400

    Add files via upload

---
+++ src/main/java/com/polentzi/calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package com.polentzi.calculator;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		Calculator testCalc1 = new Calculator(10.5,"+",5.2);
+		testCalc1.performOperation();
+		testCalc1.getResults();
+	}
+}
